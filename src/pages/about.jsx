8e7a28cc00b1ec2d7f091ef1bf370bf4aad03309
Timothy Eich <94193381+TimothyEich@users.noreply.github.com>
2023-07-30 12:14:47 -0500

    Fixed Icon hover color when in dark mode.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -17,7 +17,7 @@ function SocialLink({ className, href, children, icon: Icon }) {
       <Link
         href={href}
         className="group flex text-sm font-medium text-zinc-800 transition hover:text-yellow-500 dark:text-zinc-200 dark:hover:text-pink-500">
-        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-yellow-500"/>
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-yellow-500 dark:group-hover:fill-pink-500"/>
         <span className="ml-4">{children}</span>
       </Link>
     </li>
